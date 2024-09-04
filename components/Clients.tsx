c9b42bc5e154670c97c5c7dc55a47b69c8cb5d09
Finsi19 <zfino52@gmail.com>
2024-09-04 17:20:11 +0700

    1

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -27,17 +27,17 @@ const Clients = () => {
 
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map((company) => (
-            <React.Fragment key={company.id}>
+            <React.Fragment key={company}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
                 <img
-                  src={company.img}
-                  alt={company.name}
+                  src={company}
+                  alt={company}
                   className="md:w-10 w-5"
                 />
                 <img
-                  src={company.nameImg}
-                  alt={company.name}
-                  width={company.id === 4 || company.id === 5 ? 100 : 150}
+                  src={company}
+                  alt={company}
+                  width={company === 4 || company === 5 ? 100 : 150}
                   className="md:w-24 w-20"
                 />
               </div>
